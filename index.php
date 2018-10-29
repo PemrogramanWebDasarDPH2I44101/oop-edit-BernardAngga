98fbe186439e14fe2b36da0ad028119cc2afebd2
Bernard Angga Sihotang <42725315+BernardAngga@users.noreply.github.com>
2018-10-29 21:55:58 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
-    <form action="class.php" method="post">
-        Masukan angka 1<input type="text" name="input1"><br>
-        Masukan angka 2<input type="text" name="input2"><br>
+    <form action="Class.php" method="post">
+        Masukan nim<input type="text" name="NIM"><br>
+        Masukan nama<input type="text" name="nama"><br>
         Pilih Operasi
         <select name="operasi">
             <option value="+">+</option>
